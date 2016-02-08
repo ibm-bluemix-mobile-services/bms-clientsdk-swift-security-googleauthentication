c30c4e619af53af7cf409fa6a350972734e1caf2
alon24 <htpcfreak@gmail.com>
2016-02-08 10:25:06 +0200

    initial xcode

---
+++ BMSGoogleAuthentuication/BMSGoogleAuthentuication.h
@@ -0,0 +1,19 @@
+//
+//  BMSGoogleAuthentuication.h
+//  BMSGoogleAuthentuication
+//
+//  Created by Ilan Klein on 08/02/2016.
+//  Copyright © 2016 ibm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMSGoogleAuthentuication.
+FOUNDATION_EXPORT double BMSGoogleAuthentuicationVersionNumber;
+
+//! Project version string for BMSGoogleAuthentuication.
+FOUNDATION_EXPORT const unsigned char BMSGoogleAuthentuicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSGoogleAuthentuication/PublicHeader.h>
+
+
